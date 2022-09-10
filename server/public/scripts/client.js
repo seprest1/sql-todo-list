4f6c675bad3bcc92d4568cde65f44389a1ab37cb
Sarah Preston <seprest@gmail.com>
2022-09-10 14:55:43 -0500

    connected server to client side, tested it.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,14 +3,14 @@ console.log('in js');
 $(document).ready(clickHandlers);
 
 function clickHandlers(){
-    $('#submitButton').on('click', addTask);
+    $('#submitButton').on('click', getTasks);
     $('#editButton').on('click', editCategory);
     $(document).on('click', '.checkButton', taskComplete);
     $(document).on('click', '.deleteButton', deleteTask);
+    renderTasks();
 };
 
 function addTask(){
-
 };
 
 function editCategory(){
@@ -25,6 +25,17 @@ function deleteTask(){
 
 };
 
-function renderTasks(){
+function getTasks(){
+    console.log('you clicked me!');
+    $.ajax({
+        method: 'GET',
+        url: '/tasks'
+    }).then((taskRes) => {
+        renderTasks(taskRes);
+    });
+}
 
+
+function renderTasks(){
+    console.log('hi!');
 };
