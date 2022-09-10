2eb7e1494adc332924a1376ba8f3a166618813fa
Sarah Preston <seprest@gmail.com>
2022-09-10 14:21:58 -0500

    created architecture of client-side functions and click listeners

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,30 @@
 console.log('in js');
 
-$(document).ready(function(){
-    console.log('jQuery is working!');
-});
+$(document).ready(clickHandlers);
+
+function clickHandlers(){
+    $('#submitButton').on('click', addTask);
+    $('#editButton').on('click', editCategory);
+    $(document).on('click', '.checkButton', taskComplete);
+    $(document).on('click', '.deleteButton', deleteTask);
+};
+
+function addTask(){
+
+};
+
+function editCategory(){
+
+};
+
+function taskComplete(){
+
+};
+
+function deleteTask(){
+
+};
+
+function renderTasks(){
+
+};
