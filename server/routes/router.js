d3c43233591bf65bbe112fe0d465d17bae37ba5b
Sarah Preston <seprest@gmail.com>
2022-09-10 17:24:31 -0500

    POST route working, able to add task item into database.

--- server/routes/router.js
+++ server/routes/router.js
@@ -17,5 +17,29 @@ router.get('/', (req, res) => {
     });
 });
 
+router.post('/', (req, res) => {
+    console.log(req.body);
+    const listItem = req.body.listItem;
+    const checked = req.body.checked;
+    const timeCompleted = req.body.timeCompleted;
+
+    const queryText = `
+    INSERT INTO "tasks"
+    ("listItem", "checked", "timeCompleted")
+    VALUES
+    ($1, $2, $3);
+    `;
+
+    const sqlValues = [listItem, checked, timeCompleted];
+   
+    db.query(queryText, sqlValues)
+        .then(result => {
+        res.send(200);
+    })
+    .catch(error => {
+        console.log('error adding tasks', error);
+        res.sendStatus(500);
+    })
+});
 
 module.exports = router;
