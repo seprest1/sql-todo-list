429cee8a1469a728e8ae4f06e097c55c562f94b4
Sarah Preston <seprest@gmail.com>
2022-09-11 12:51:11 -0500

    did some final editiing with logic

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,9 +4,9 @@ $(document).ready(clickHandlers);
 
 function clickHandlers(){
     $('#submitButton').on('click', addTask);
-    $('#editButton').on('click', editCategory);
     $(document).on('click', '.checkButton', taskComplete);
-    $(document).on('click', '.deleteButton', deleteTask);
+    $(document).on('click', '#deleteButton', deleteAlert);
+    $(document).on('click', '#editButton', editCategory);
     getTasks();
 };
 
@@ -32,7 +32,26 @@ function addTask(){
 };
 
 function editCategory(){
+    $('#listTitle').empty();
+    $('#listTitle').append(`
+        <th class="col-10" id="newCategory">
+            <input type="text" id="newCategory" placeholder="New Title..." required/>
+        </th>
+        <th class="col-1">
+            <button type="button" id="submitTitleButton" class="btn btn-outline-secondary btn-sm mx-3">Submit</button>
+        </th>`);
+    $(document).on('click', '#submitTitleButton', renderTitle);
+};
 
+function renderTitle(){
+    let newTitle = $("#newCategory").val();
+    console.log(newTitle);
+    $('#listTitle').empty();
+    $('#listTitle').append(`
+        <th class="col-10" id="taskCategory">${newTitle}</th>
+        <th class="col-1">
+            <button type="button" id ="editButton" class="btn btn-outline-secondary btn-sm mx-3">Edit</button>
+        </th>`);
 };
 
 function taskComplete(){
@@ -63,25 +82,40 @@ function getTasks(){
 }
 
 function renderTasks(response){
-    $('#tableBody').empty();
+    $('#listItems').empty();
     for (let task of response){
-        $('#tableBody').append(`
+        if (task.checked === true){
+            $('#listItems').append(`
             <tr data-id="${task.id}">
                 <td>
-                    <input type="checkbox" value="" class="checkButton"/>
+                    <input type="checkbox" class="checkButton" checked="true"/>
+                </td>
+                <td class="checkedListItem" data-tf="${task.checked}">${task.listItem}</td>
+                <td>
+                    <button type="button" class="btn btn-outline-secondary btn-sm" id="deleteButton"> ✗ </button>
+                </td>
+            </tr>
+            `);
+            $('.checkedListItem').css('text-decoration', 'line-through');
+        }
+        else {
+            $('#listItems').append(`
+            <tr data-id="${task.id}">
+                <td>
+                    <input type="checkbox" class="checkButton" id="checkbox"/>
                 </td>
                 <td data-tf="${task.checked}">${task.listItem}</td>
                 <td>
-                    <button class="deleteButton">X</button>
+                    <button type="button" class="btn btn-outline-secondary btn-sm" id="deleteButton"> ✗ </button>
                 </td>
             </tr>
-        `);
+            `);
+        }
     }
 };
 
-function deleteTask(){
-    let taskToDelete = $(this).closest('tr').data('id');
-    console.log(taskToDelete);
+
+function deleteTask(taskToDelete){
     $.ajax({
         method: 'DELETE',
         url: `/tasks/${taskToDelete}`
@@ -93,3 +127,25 @@ function deleteTask(){
     })
 };
 
+function deleteAlert(){
+    let taskToDelete = $(this).closest('tr').data('id');
+    console.log(taskToDelete);
+
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, it will be removed permanently!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+    })
+    .then((willDelete) => {
+        if (willDelete) {
+            swal("Poof! The task has been deleted!", {
+            icon: "success",});
+            deleteTask(taskToDelete);
+        } 
+        else {
+            swal("Your task is safe!");
+        }
+    });
+};
