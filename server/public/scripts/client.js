8a83a593fd4b5b7e6d3403e26bbace5f1cc6a59e
Sarah Preston <seprest@gmail.com>
2022-09-11 14:43:36 -0500

    final CSS and edited readme

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,7 +6,6 @@ function clickHandlers(){
     $('#submitButton').on('click', addTask);
     $(document).on('click', '.checkButton', taskComplete);
     $(document).on('click', '#deleteButton', deleteAlert);
-    $(document).on('click', '#editButton', editCategory);
     getTasks();
 };
 
@@ -31,28 +30,7 @@ function addTask(){
     });
 };
 
-function editCategory(){
-    $('#listTitle').empty();
-    $('#listTitle').append(`
-        <th class="col-10" id="newCategory">
-            <input type="text" id="newCategory" placeholder="New Title..." required/>
-        </th>
-        <th class="col-1">
-            <button type="button" id="submitTitleButton" class="btn btn-outline-secondary btn-sm mx-3">Submit</button>
-        </th>`);
-    $(document).on('click', '#submitTitleButton', renderTitle);
-};
 
-function renderTitle(){
-    let newTitle = $("#newCategory").val();
-    console.log(newTitle);
-    $('#listTitle').empty();
-    $('#listTitle').append(`
-        <th class="col-10" id="taskCategory">${newTitle}</th>
-        <th class="col-1">
-            <button type="button" id ="editButton" class="btn btn-outline-secondary btn-sm mx-3">Edit</button>
-        </th>`);
-};
 
 function taskComplete(){
     let taskToCheck = $(this).closest('tr').data('id');
@@ -61,8 +39,6 @@ function taskComplete(){
         method: 'PUT',
         url: `/tasks/${taskToCheck}`
     }).then((response) => {
-        $(this).val('on');
-        console.log($(this).next('td'));
         getTasks();
     }).catch ((error) => {
         console.log('Error in task PUT', error);
@@ -86,7 +62,7 @@ function renderTasks(response){
     for (let task of response){
         if (task.checked === true){
             $('#listItems').append(`
-            <tr data-id="${task.id}">
+            <tr data-id="${task.id}" class="hoverTr">
                 <td>
                     <input type="checkbox" class="checkButton" checked="true"/>
                 </td>
@@ -100,11 +76,11 @@ function renderTasks(response){
         }
         else {
             $('#listItems').append(`
-            <tr data-id="${task.id}">
+            <tr data-id="${task.id}" class="hoverTr">
                 <td>
                     <input type="checkbox" class="checkButton" id="checkbox"/>
                 </td>
-                <td data-tf="${task.checked}">${task.listItem}</td>
+                <td data-tf="${task.checked}" class="taskItem">${task.listItem}</td>
                 <td>
                     <button type="button" class="btn btn-outline-secondary btn-sm" id="deleteButton"> ✗ </button>
                 </td>
@@ -149,3 +125,34 @@ function deleteAlert(){
         }
     });
 };
+
+
+
+
+
+
+//////////////////////////////////////////////////WIP///////////////////////////////////////////////////////////
+// $(document).on('click', '#editButton', editCategory);
+//
+// function editCategory(){
+//     $('#listTitle').empty();
+//     $('#listTitle').append(`
+//         <th class="col-10" id="newCategory">
+//             <input type="text" id="newCategory" placeholder="New Title..." required/>
+//         </th>
+//         <th class="col-1">
+//             <button type="button" id="submitTitleButton" class="btn btn-outline-secondary btn-sm mx-3">Submit</button>
+//         </th>`);
+//     $(document).on('click', '#submitTitleButton', renderTitle);
+// };
+
+// function renderTitle(){
+//     let newTitle = $("#newCategory").val();
+//     console.log(newTitle);
+//     $('#listTitle').empty();
+//     $('#listTitle').append(`
+//         <th class="col-10 col-8-lg col-6-md col-4-sm" id="taskCategory">${newTitle}</th>
+//         <th class="col-1">
+//             <button type="button" id ="editButton" class="btn btn-outline-secondary btn-sm mx-3">Edit</button>
+//         </th>`);
+// };
